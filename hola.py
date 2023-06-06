48de7ca1b2319707eb6096d071ca8b1504beb3be
daDev <daaguirre0215@gmail.com>
2023-06-06 17:13:09 -0600

    Se modifico el gitignore

---
+++ hola.py
@@ -0,0 +1,7 @@
+import random
+
+# Generar un número aleatorio entre 1 y 100
+numero_aleatorio = random.randint(1, 100)
+
+# Imprimir el número aleatorio generado
+print("Número aleatorio:", numero_aleatorio)
